48eb7eb0e246c9d0b9904ffcbc1e201112e8df9f
Anna_Ponomareva <trivi-ja@mail.ru>
2022-10-18 00:18:01 +0300

    Схема

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string[] CreateArrayFromArr(string[] array)
+{
+    int count = 0;
+    string[] result = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            result[count] = array[i];
+            count ++;
+        }
+    }
+    Array.Resize(ref result, count);
+    return result;
+}
